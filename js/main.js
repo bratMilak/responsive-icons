cd430db79a4cabb9b0ab0dd1b6a10eb905272327
bratMilak <milakovicpetar@gmail.com>
2019-09-02 11:55:49 +0200

    Js upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+(function() {
+
+	// Find all icomoon elements
+	var allElements = $('[class^="icon-"]');
+
+	allElements.each(function() {
+		// Get full DOM path of element
+		var fullPath = [],
+		    elm,
+		    entry;
+
+		for (elm = this; elm; elm = elm.parentNode) {
+		    entry = elm.tagName.toLowerCase();
+		    if (entry === "html") {
+		      break;
+		    }
+		    if (elm.className) {
+		      entry += "." + elm.className.replace(/ /g, '.');
+		    }
+		    fullPath.push(entry);
+		}
+		fullPath.reverse();
+		var fontPathOutput = fullPath.join(" ");     
+		
+
+		var $this = $(this),
+			fullElPath = fullPath.join(" "),
+			elClass = $this.attr('class').split(' ').join('.'),
+		    elementType = $this.prop('nodeName').toLowerCase(),
+		    target =   elementType + '.' + elClass;
+		   
+
+		// console.log(elClass);
+		// console.log(elementType);
+		// console.log(target);
+		console.log(fontPathOutput);
+
+		var fontValuePixels = getComputedStyle(document.querySelector(fontPathOutput), ':before').getPropertyValue('font-size'),
+			fontValue = parseInt(fontValuePixels, 10);
+
+		if(fontValue >= 60){
+        	$this.addClass('big-font');
+        }
+        if((fontValue < 60) & (fontValue > 22)){
+        	$this.addClass('med-font');
+        }
+        if( (fontValue < 22)){
+        	$this.addClass('small-font');
+        } 	
+		console.log(getComputedStyle(document.querySelector(fontPathOutput), ':before').getPropertyValue('font-size'));
+	});
+
+
+}());
